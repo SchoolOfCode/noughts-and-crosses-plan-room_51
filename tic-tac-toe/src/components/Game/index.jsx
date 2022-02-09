c32873fedc9f4342e62e168d0c92e5084f9f6a46
21ciprian <2021ciprianoffice@gmail.com>
2022-02-09 15:54:52 +0000

    Displayed winner

--- tic-tac-toe/src/components/Game/index.jsx
+++ tic-tac-toe/src/components/Game/index.jsx
@@ -2,18 +2,9 @@ import React, {useState} from 'react'
 import Board from '../Board'
 
 function Game() {
-	const [board, setBoard] = useState([
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-		null,
-	])
+	const [board, setBoard] = useState(Array(9).fill(null))
 	const [xIsNext, setXIsNext] = useState(true)
+
 	function handleClick(e) {
 		let id = e.target.id
 		let updatedSquares = board.slice()
@@ -21,20 +12,43 @@ function Game() {
 		if (board[id] === null) {
 			updatedSquares[id] = xIsNext ? 'X' : 'O'
 			setBoard(updatedSquares)
-			console.log('board if :', board)
 			setXIsNext(!xIsNext)
-			console.log('xIsNext:', xIsNext)
 		}
-		console.log('board else :', board)
-
-		console.log('e.target: ', e.target.id)
 	}
-	// function move(e) {
-	//   if(e.target)
-	// }
+	const winner = calculateWinner(board)
+	function calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		]
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i]
+			if (
+				squares[a] &&
+				squares[a] === squares[b] &&
+				squares[a] === squares[c]
+			) {
+				return squares[a]
+			}
+		}
+		return null
+	}
+
 	return (
 		<div>
-			{xIsNext ? <p>Player 1(X) is next</p> : <p>Player 2(O) is next</p>}
+			{winner ? (
+				<p> Winner: {winner}</p>
+			) : xIsNext ? (
+				<p>Player 1(X) is next</p>
+			) : (
+				<p>Player 2(O) is next</p>
+			)}
 			<Board board={board} xIsNext={xIsNext} handleClick={handleClick} />
 		</div>
 	)
